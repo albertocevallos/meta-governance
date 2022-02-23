0367ea5f29416986d9ddb02a262e3c23cf4140c7
Alberto Cevallos <acvllsh@gmail.com>
2022-02-23 18:00:54 -0500

    feat: :memo: base deployment script

--- scripts/1_deploy.py
+++ scripts/1_deploy.py
@@ -3,6 +3,7 @@ from brownie import (
     accounts,
     network,
     interface,
+    MetaGovernance
 )
 import os
 import sys
@@ -16,21 +17,30 @@ def main():
     # setup
     dev = connect_account()
 
-    # args
-
     owner = dev
-    token = ''
-    name = ''
-    symbol = ''
-    tokens = []
-    multipes = ''
+    token = '0xD533a949740bb3306d119CC777fa900bA034cd52' # eg. CRV
+    name = 'Meta Governance CRV' # Meta Governance + {YOUR_TOKEN}
+    symbol = 'metaCRV' # eg. for CRV ecosystem
+    tokens = ['0xD533a949740bb3306d119CC777fa900bA034cd52', '0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2'] # eg. CRV, veCRV
+    multiples = [1e18, 4e18] # 1 vote == 1e18
+    isLp = [False, False]
 
+    args = (owner, token, name, symbol, tokens, multiples, isLp)
 
     # deploy
+    governance = deploy_governance(dev, args)
 
     # wireup
 
 
+def deploy_governance(dev, args):
+    (owner, token, name, symbol, tokens, multiples, isLp) = args
+    governance_instance = MetaGovernance.deploy(owner, token, name, symbol, tokens, multiples, isLp, {'from': dev})
+    console.print('[blue]Meta Governance contract was deployed at: [/blue]', governance_instance.address)
+    return governance_instance
+
+
+    # de;poy instance
 
 def connect_account():
     console.print(f"\nnetwork: {network.show_active()}")
